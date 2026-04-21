abc38a0255d3039d91f0b5da5ef83b59ff0b4524
bhuvanahareem <bhuvanahareendran@gmail.com>
2026-04-21 21:07:24 +0530

    Changed port

--- app.py
+++ app.py
@@ -386,5 +386,5 @@ async def serve_index():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
